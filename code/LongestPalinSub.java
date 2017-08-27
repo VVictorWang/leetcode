be36bafafde66ec6162d8425d7149f0f20a890d9
Victor <643705913@qq.com>
2017-08-27 13:22:53 +0800

    add LongestPalinSub.java

---
+++ code/LongestPalinSub.java
@@ -0,0 +1,53 @@
+/**
+ * Created by victor on 8/27/17.
+ * email: chengyiwang@hustunque.com
+ */
+
+/*
+* Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ Example:
+ Input: "babad"
+ Output: "bab"
+ Note: "aba" is also a valid answer.
+ Example:
+ Input: "cbbd"
+ Output: "bb"
+* */
+public class LongestPalinSub {
+
+    public static String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandPalin(s, i, i);
+            int len2 = expandPalin(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandPalin(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        String s = "rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlmwnbxvmgvllafrpmlfuqpbhjddmhmbcgmlye" +
+                "ypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtuwvvvwnqlowdihtrdzjffrgoeqivnprdnpvf" +
+                "juhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsfgjadicwzycswwmpluvzqdvnhkcofptq" +
+                "rzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqiozworibeycuarcidbljslwbalcemgy" +
+                "mnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmcvtdsvehewrvtkeqwgmatqdpwl" +
+                "ayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycdcscioimenaeohgpbcpogyif" +
+                "csatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoanhyfkgwaelsfdeeviqogj" +
+                "presnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbgqtcqulxodurugofuwzu" +
+                "dnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngsemcolrnlyhqlbqbce" +
+                "stadoxtrdvcgucntjnfavylip";
+        System.out.println(longestPalindrome(s));
+    }
+}
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # !bin/sh
 
-FILE_NAME=addTwoNumbers.java
+FILE_NAME=LongestPalinSub.java
 TARGET_NAME=${FILE_NAME%.*}
 SOURCE_DIR=./code
 TARGET_DIR=./bin
